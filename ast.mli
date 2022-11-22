c41c440d484ae8f55018cb778dce152e99c3a75d
powcoder <powcoder@163.com>
2022-11-22 12:15:13 +0800

    COMS W4115 HW2 OCaml

---
+++ ast.mli
@@ -0,0 +1,12 @@
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+type operator = Add | Sub | Mul | Div
+
+type expr =
+    Binop of expr * operator * expr
+  | Lit of int
+  | Seq of expr * expr
+  | Asn of string * expr
+  | Var of string
---
+++ scanner.mll
@@ -0,0 +1,10 @@
+{ open Parser }
+
+rule tokenize = parse
+  [' ' '\t' '\r' '\n'] { tokenize lexbuf }
+| '+' { PLUS }
+| '-' { MINUS }
+| '*' { TIMES }
+| '/' { DIVIDE }
+| ['0'-'9']+ as lit { LITERAL(int_of_string lit) }
+| eof { EOF }
